1afafe078ab75c223e99eb0104835ba0b901647a
Ashutosh Raj <mr.ashutoshraj@gmail.com>
2021-08-08 17:42:14 +0530

    changed to streamlit

---
+++ first_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+
+st.image('./pic.jpeg')
+st.title("Hi, I am Ashutosh Raj! :sunglasses:")
+# st.header("PROFILE SUMMARY")
+
+st.header("I am working on the industrial asset's health analysis associated to Power, Oil and Gas domain.",
+             anchor=None)
+st.subheader("⦿ Experienced in Object Detection, Image Alignment, Core Image Processing, Point Cloud Processing,"
+             " Design Patterns, Actor System(Thespian), Differential Privacy, and Federated Learning.", anchor=None)
+st.subheader("⦿ Solid academic background in signal processing and statistics.", anchor=None)
+st.subheader("⦿ Proficient in Python and most of the data related packages, libraries,"
+             " frameworks except web development.", anchor=None)
+st.subheader("⦿ Ability to present insights and communicate with non-data professionals.", anchor=None)
+st.subheader("⦿ I spend around 20 hrs/week on improving my Analytical skills.", anchor=None)
+st.subheader("⦿ My interests include unlocking the power of pattern recognition in building "
+             "meaningful and impactful solutions for the industry.", anchor=None)
+
+st.caption("Drop me an email/message, if want to discuss anything related to my experience. :)")
+
+st.caption("Email: mr.ashutoshraj@gmail.com")
